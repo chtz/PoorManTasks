f2931a5d26e3b9611d7f069c68ac5f374069e2e9
Christian Tschenett <chtz@me.com>
2017-02-09 22:13:02 +0100

    thymeleaf sample added

--- src/main/java/ch/furthermore/pmt/Application.java
+++ src/main/java/ch/furthermore/pmt/Application.java
@@ -1,14 +1,14 @@
 package ch.furthermore.pmt;
 
-import java.io.IOException;
-
-import javax.script.ScriptException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Build Samples:
@@ -25,11 +25,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 @Controller
 public class Application {
-	@RequestMapping("/")
-	@ResponseBody
-	String home() throws NoSuchMethodException, IOException, ScriptException {
-		return "nothing to see here";
+	@ModelAttribute("items")
+	public List<String> populateItems() {
+	    return Arrays.asList(new String[]{"version: 1.0","hash: " + System.identityHashCode(this)});
 	}
+	
+	@RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("title", "nothing to see here (" + System.identityHashCode(this) + ")");
+        return "index";
+    }
 	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
